0ec29a20a3bf238bb0d7d5c5eefae59707691ddc
mbamber1986 <30671636+mbamber1986@users.noreply.github.com>
2025-10-25 16:47:37 +0100

    Added WhiteList support for Handler and all relitive files, Chaneged Method names

--- src/CoreFiles/HandlerCore.php
+++ src/CoreFiles/HandlerCore.php
@@ -1,15 +1,15 @@
 <?php
 namespace LazarusPhp\OpenHandler\CoreFiles;
 
- class HandlerCore
+use Exception;
+
+ abstract class HandlerCore
 {
 
     protected static $directory = "";
     protected static $prefix = "";
 
-
-
-
+    
     /**
      * Require Access Token in order to Continue Prevent Calling methods directly;
      * 
@@ -20,7 +20,7 @@ namespace LazarusPhp\OpenHandler\CoreFiles;
      * @return bool
      */
 
-    protected static function hasDirectory(string $path):bool
+    protected function hasDirectory(string $path):bool
    {
         return is_dir($path) ? true : false;
    }
@@ -32,8 +32,9 @@ namespace LazarusPhp\OpenHandler\CoreFiles;
     * @property string $path;
     * @return bool 
     */
-     protected static function hasFile(string $path):bool
+     protected function hasFile(string $path):bool
     {
+       
         return (is_file($path)) ? true : false;
     }
 
@@ -42,7 +43,7 @@ namespace LazarusPhp\OpenHandler\CoreFiles;
      * @property string $path;
      * @return bool
      */
-     protected static function fileExists(string $path):bool
+     protected function fileExists(string $path):bool
     {
         return (file_exists($path)) ? true : false;
     }
@@ -51,8 +52,9 @@ namespace LazarusPhp\OpenHandler\CoreFiles;
      * this method will be used to detect the directories and any prefix Attched to the file
      */
 
-     protected static function useDirectory($directory)
+     protected static function filepath($directory)
     {
+       
         $root = self::$directory;
         $prefix = self::$prefix ?? "";
         return $root.$prefix.$directory;
@@ -60,6 +62,7 @@ namespace LazarusPhp\OpenHandler\CoreFiles;
 
      protected static function validMode(int $mode)
     {
+       
         $modes = [0600, 0644, 0664, 0700, 0755, 0777];
         if (in_array($mode, $modes)) {
             return true;
@@ -75,8 +78,9 @@ namespace LazarusPhp\OpenHandler\CoreFiles;
      *  @property string $path
      *  @return bool
      */
-     protected static function withDots($path):bool
+     protected function withDots($path):bool
     {
+       
         return ($path === "." || $path === "..") ? true : false;
     }
 
@@ -85,30 +89,32 @@ namespace LazarusPhp\OpenHandler\CoreFiles;
      * @property string $path
      * @return bool
      */
-     protected static function writable(string $path): bool
+     protected function writable(string $path): bool
     {
+       
         return is_writable($path) ? true : false;
     }
 
-     protected static function readable(string $path):bool
+     protected  function readable(string $path):bool
     {
+       
         return is_readable($path) ? true : false;
     }
 
 
     // Crud Elements
 
-     protected static function addDirectory(string $path,int $mode = 0755, bool $recursive = true)
+     protected function generateDirectory(string $path,int $mode = 0755, bool $recursive = true)
     {
         // Detect if  directory Exists
-        $path = self::useDirectory($path);
-        if(self::validMode($mode) === true)
+        $path = $this->filePath($path);
+        if($this->validMode($mode) === true)
         {
         // Create Folder if it doesnt exist
-            if(self::hasDirectory($path) === false)
+            if($this->hasDirectory($path) === false)
             {
                    $oldUmask = umask(0);
-                    if (!mkdir($path,$mode,$recursive) && !self::hasDirectory($path)) {
+                    if (!mkdir($path,$mode,$recursive) && !$this->hasDirectory($path)) {
                         umask($oldUmask);
                         throw new \RuntimeException("Failed to create directory: {$path}");
                     }
@@ -122,15 +128,33 @@ namespace LazarusPhp\OpenHandler\CoreFiles;
         }
     }
 
-
-    protected function listAll(string $path, bool $recursive = false):array
+    protected function generatePrefix(string $path,callable $handler,array $middleware=[])
     {
+        
+        if(self::$prefix === ""){
+        self::$prefix = $path;
+        }
+        // Add MiddleWare Option here
+
+        if (is_callable($handler)) {
+            $class = new Handler();
+            $handler($class,$path);
+            self::$prefix = "";
+        }
+        // Reset Prefix to start a new oneself
+        return null;
+    }
+
+    protected function generateList($path, bool $recursive =true,$files=true):array
+    {
+       
         // Avoid double prefixing
         if (!empty(self::$directory) && strpos($path, self::$directory) !== 0) {
             $path = rtrim(self::$directory, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . ltrim($path, DIRECTORY_SEPARATOR);
         }
 
-        if (self::hasDirectory($path) === false) {
+
+        if ($this->hasDirectory($path) === false) {
             return ["folders" => [], "files" => []];
         }
 
@@ -140,27 +164,33 @@ namespace LazarusPhp\OpenHandler\CoreFiles;
 
         foreach ($items ?: [] as $item) {
 
-            if (self::withDots($item))  continue;
+            if ($this->withDots($item))  continue;
 
             $ds = DIRECTORY_SEPARATOR;
             $fullpath = rtrim($path, $ds) . $ds . ltrim($item, $ds);
 
-            if (self::hasFile($fullpath) === true) {
-                $result["files"][] = $fullpath;
-            } elseif (self::hasDirectory($fullpath) === true) {
-                $result["folders"][] = $fullpath;
+            if($files === true){
+                if ($this->hasFile($fullpath) === true) {
+                    $result["files"][] = $fullpath;
+                }
+            }
+            
+            if($recursive === true){
+                if ($this->hasDirectory($fullpath) === true) {
+                    $result["folders"][] = $fullpath;
 
-                // Check if Request is set as recursive;
-               
-                    // Recursively list contents of subdirectories
-                    $subdir = self::listAll($fullpath,$recursive);
+                    // Check if Request is set as recursive;
+                
+                        // Recursively list contents of subdirectories
+                        $subdir = $this->generateList($fullpath,$recursive);
 
-                    // Ensure recursion always returns an array with both keys
-                    $subFolders = $subdir["folders"] ?? [];
-                    $subFiles = $subdir["files"] ?? [];
+                        // Ensure recursion always returns an array with both keys
+                        $subFolders = $subdir["folders"] ?? [];
+                        $subFiles = $subdir["files"] ?? [];
 
-                    $result["folders"] = array_merge($result["folders"], $subFolders);
-                    $result["files"] = array_merge($result["files"], $subFiles);
+                        $result["folders"] = array_merge($result["folders"], $subFolders);
+                        ($files ===true) ? $result["files"] = array_merge($result["files"], $subFiles) : false;
+                }
             }
         }
 
@@ -168,15 +198,22 @@ namespace LazarusPhp\OpenHandler\CoreFiles;
         return $result;
     }
 
-
-    protected function addFile(string $filename, int|string|array $data,int $flags=0)
+    protected function generateBreadcrumb()
     {
+       
+        $path = ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET["path"])) ? $_GET["path"] : self::$directory;
+        return htmlspecialchars($path);
+    }
+
+    protected function generateFile(string $filename, int|string|array $data,int $flags=0)
+    {
+       
         $supportedFiles = ["php","txt","tpl","class","env","json"];
         $extension = pathinfo($filename)["extension"];
         if(in_array($extension,$supportedFiles))
         {
             if(substr($filename,0,1) === DIRECTORY_SEPARATOR){
-                $filename = self::useDirectory($filename);
+                $filename = $this->filePath($filename);
                 if(!file_exists($filename))
                 {
                     $output = $data;
@@ -202,13 +239,14 @@ namespace LazarusPhp\OpenHandler\CoreFiles;
 
 
 
-    protected function deleteData(string $path): bool
+    protected function generateDelete(string $path): bool
     {
-        $path = self::useDirectory($path);
-        if (self::hasFile($path)) {
+       
+        $path = $this->filePath($path);
+        if ($this->hasFile($path)) {
             return unlink($path);
-        } elseif (self::hasDirectory($path)) {
-            $items = $this->listAll($path, true);
+        } elseif ($this->hasDirectory($path)) {
+            $items = $this->generateList($path, true);
             foreach ($items['files'] as $file) {
                 @unlink($file);
             }
--- src/Interfaces/HandlerInterface.php
+++ src/Interfaces/HandlerInterface.php
@@ -3,10 +3,13 @@ namespace LazarusPhp\OpenHandler\Interfaces;
 
 interface HandlerInterface
 {
-    public function list(string $directory,bool $recursive=true);
-    public function directory(string $path,int $mode=0755, bool $recursive=true):bool;
+    public function list(string $directory,bool $recursive=true,$files=true);
+    public function directory(string $path,int $mode=0755, bool $recursive=true);
     public function delete(string $path);
     public function file(string $path, int|string|array $data,int $flags=0);
     public function prefix(string $path, callable $handler,array $middleware=[]);
     public function setDirectory($directory="");
+    public function upload(string $path,callable $handler);
+    public function breadcrumb();
 }
+
