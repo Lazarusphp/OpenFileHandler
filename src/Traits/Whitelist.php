e49a8be2ce959b55289e6960efb78b1f05cc21b8
mbamber1986 <30671636+mbamber1986@users.noreply.github.com>
2025-10-29 15:41:01 +0000

    Added Support for method Whitelisting

--- src/Traits/Whitelist.php
+++ src/Traits/Whitelist.php
@@ -14,14 +14,47 @@ trait Whitelist
         return method_exists($this,$method) ? true : false;
     }
 
-    public function MethodWhitelist(string $method)
+    public function validMethod(string $method)
     {
-
+        if($this->hasMethod($method)=== true)
+        {
+            if(isset($this->allowedMethods[$method])){
+                if(is_array($this->allowedMethods) && in_array($method,$this->allowedMethods))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
+    
     
     private function validHelper(string $method):bool
     {
        return (in_array($method,$this->allowedHelpers) && is_array($this->allowedHelpers)) ? true : false;
+    }
+
+    public function setMethodWhitelist(string|array $method)
+    {
+      $count = is_array($method) ? count($method) : 1;
+        $args = is_array($method) ? $method[0] : $method;
+        if($count > 1)
+        {
+            foreach($args as $method)
+            {
+                if($this->validMethod($method) === false)
+                {
+                    throw new \Exception("Access to method $method is denied.");
+                }
+            }
+        }
+        else
+        {
+            if($this->validMethod($args) === false)
+            {
+                throw new \Exception("Access to method $args is denied.");
+            }
+        }
     }
 
     protected function setWhitelist(string|array $method)
@@ -32,9 +65,9 @@ trait Whitelist
         {
             foreach($args as $method)
             {
-                if($this->validHelper($method) === false)
+                if($this->validHelper($args) === false)
                 {
-                    throw new \Exception("Access to method $method is denied.");
+                    throw new \Exception("Access to method Helper $args is denied.");
                 }
             }
         }
