921a075075831cd67ba6ae0618c2f3a2d0dd7328
mbamber1986 <30671636+mbamber1986@users.noreply.github.com>
2025-10-31 14:51:33 +0000

    Updated ImageHandler, removed Interface Reference and added Constructor

--- src/CoreFiles/ImageHandler.php
+++ src/CoreFiles/ImageHandler.php
@@ -3,14 +3,22 @@ namespace LazarusPhp\OpenHandler\CoreFiles;
 
 use LazarusPhp\OpenHandler\Interfaces\ImageInterface;
 
-class ImageHandler  extends HandlerCore implements ImageInterface
+class ImageHandler  extends HandlerCore
 {
 
+    protected array $allowedHelpers = [];
+    protected array $allowedMethods = [""];
 
 
-
-    public function upload($path)
+    public function __construct()
     {
+        parent::__construct(__CLASS__);
+    }
+
+
+    public function upload(string $path)
+    {
+        $path = (string) $this->filePath($path);
         if($this->hasDirectory($path))
         {
             echo "Directory Exists";
