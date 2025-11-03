337c41bb4ec76eb99a1892a943bde4238a0903d9
mbamber1986 <30671636+mbamber1986@users.noreply.github.com>
2025-11-03 22:57:44 +0000

    Updated OpenHandler to support custom Handler Classes

--- src/OpenHandler.php
+++ src/OpenHandler.php
@@ -5,20 +5,44 @@ use LazarusPhp\OpenFileHandler\CoreFiles\HandlerCore;
 use LazarusPhp\OpenFileHandler\Interfaces\PermissionsInterface;
 use LazarusPhp\OpenHandler\CoreFiles\Handler;
 use LazarusPhp\OpenHandler\Interfaces\HandlerInterface;
+use ReflectionClass;
 
 class OpenHandler
 {
     private static HandlerInterface $handlerInterface;
 
-    public static function create(string $directory,$customhandler ="")
-    
+
+    private static  function reflection($classname)
     {
-        $handler =( empty($customhandler)) ? Handler::class : $customhandler;
+        return new ReflectionClass($classname);
+    }
+
+    public static function create(string $directory,array $handler =[Handler::class])
+    {
+
+        if(count($handler) === 1)
+        {
+            $handler = implode("",$handler);
+        }
+        else
+        {
+            trigger_error("Error Handler Must contain no more than one Class");
+            return false;
+        }
+
+        
+
+        $reflection = self::reflection($handler);
         if (class_exists($handler)) {
-            $handler = explode("::",$handler);
-            self::$handlerInterface = new $handler[0]($directory);
-            self::$handlerInterface->setDirectory($directory);
-            return self::$handlerInterface;
+            if($reflection->isInstantiable()){
+                self::$handlerInterface = new $handler($directory);
+                self::$handlerInterface->setDirectory($directory);
+                return self::$handlerInterface;
+            }
+            else
+            {
+                echo "Failed to instantiate new class";
+            }
         } 
     }
 
